0155f70e96375b935bcdd8c7e884a20daa04021d
Lunarelements <bradlabrador@hotmail.com>
2020-06-02 19:01:14 -0400

    Updated my battlesnake's appearance

--- server.py
+++ server.py
@@ -18,10 +18,10 @@ class Battlesnake(object):
         # TIP: If you open your Battlesnake URL in browser you should see this data
         return {
             "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
-            "color": "#888888",  # TODO: Personalize
-            "head": "default",  # TODO: Personalize
-            "tail": "default",  # TODO: Personalize
+            "author": "Lunarelements",  # Battlesnake username
+            "color": "#13807C",  # Battlesnake color
+            "head": "silly",  # Battlesnake head
+            "tail": "bolt",  # Battlesnake tail
         }
 
     @cherrypy.expose
